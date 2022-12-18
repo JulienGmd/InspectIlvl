b36a9b48ba78a2147f56138ff42d14532bd216e7
JulienGmd <55960869+JulienGmd@users.noreply.github.com>
2022-12-18 18:51:59 +0100

    Combined bag support

--- core.lua
+++ core.lua
@@ -1,80 +1,97 @@
 -- After a bag is oppened or an item was moved in a bag
 EventRegistry:RegisterCallback("ItemButton.UpdateItemContextMatching", function(event, bagId)
+  if IsCombinedBag() then
 
-  -- Loop through the slots of the bag, if the slot can be equipped, and has a greater ilvl than
-  -- the currently equipped item in the same slot, mark it with an arrow
-
-
-  -- Loop through the slots of the bag
-  local numSlots = C_Container.GetContainerNumSlots(bagId)
-  for slotId = 1, numSlots do
-
-    -- Get the item frame
-    local bagFrameId = GetBagFrameId(bagId)
-    local itemFrameId = numSlots - slotId + 1 -- Because the item frames are in reverse order
-    local itemFrame = _G["ContainerFrame" .. bagFrameId .. "Item" .. itemFrameId]
-
-    local itemLink = C_Container.GetContainerItemLink(bagId, slotId)
-    if itemLink then
-      local invSlotId = GetInvSlotId(itemLink)
-      -- If item can be equipped in
-      -- head(1), neck(2), shoulder(3), shirt(4), chest(5), waist(6), legs(7), feet(8),
-      -- wrist(9), hands(10), finger(11), finger(12), trinket(13), trinket(14), back(15),
-      -- main hand(16), off hand(17), ranged(18)
-      if invSlotId and invSlotId >= 1 and invSlotId <= 18 then
-
-        -- Compare ilvls
-        local _, _, _, itemLevel = GetItemInfo(itemLink)
-        local equippedItemLevel = GetEquippedItemLevel(invSlotId)
-
-        -- If ring, trinket or 1H weapon, compare with both slots
-        if invSlotId == 11 or invSlotId == 13 or invSlotId == 16 then
-          local equippedItemLevel2 = GetEquippedItemLevel(invSlotId + 1)
-          if equippedItemLevel2 < equippedItemLevel then
-            equippedItemLevel = equippedItemLevel2
-          end
-        end
-
-        if itemLevel > equippedItemLevel then
-          -- Show or create the arrow texture
-          if itemFrame["ILvlUpgradeTex"] then
-            itemFrame["ILvlUpgradeTex"]:Show()
-          else
-            itemFrame["ILvlUpgradeTex"] = itemFrame:CreateTexture(nil, "OVERLAY")
-            itemFrame["ILvlUpgradeTex"]:SetTexture("Interface\\MINIMAP\\MiniMap-QuestArrow")
-            itemFrame["ILvlUpgradeTex"]:SetPoint("TOPRIGHT", itemFrame, "TOPRIGHT", 6, 4)
-            itemFrame["ILvlUpgradeTex"]:SetSize(26, 26)
-          end
-
+    -- Combined bags, all bags are oppened, so check all items of all bags
+    for bagId = 0, 4 do -- Ignore the last bag (reagents)
+      local numSlots = C_Container.GetContainerNumSlots(bagId)
+      for slotId = 1, numSlots do
+        local itemFrameId = numSlots - slotId + 1 -- Because the item frames are in reverse order
+        local itemFrame = _G["ContainerFrame" .. bagId + 1 .. "Item" .. itemFrameId]
+        local itemLink = C_Container.GetContainerItemLink(bagId, slotId)
+        if IsILvlUpgrade(itemLink) then
+          ShowArrowTexture(itemFrame)
         else
-          HideArrowFrame(itemFrame)
+          HideArrowTexture(itemFrame)
         end
-      else
-        HideArrowFrame(itemFrame)
       end
-    else
-      HideArrowFrame(itemFrame)
     end
+
+  else
+
+    -- Not combined bags, the event is called for a specific bag, check all items of this bag
+    if bagId == 5 then return end -- Ignore the last bag (reagents)
+    -- Here bagFrameId can't be deduced from the bagId,
+    -- because bagFrameId depends on the order we open the bags
+    local bagFrameId = GetBagFrameId(bagId)
+    local numSlots = C_Container.GetContainerNumSlots(bagId)
+    for slotId = 1, numSlots do
+      local itemFrameId = numSlots - slotId + 1 -- Because the item frames are in reverse order
+      local itemFrame = _G["ContainerFrame" .. bagFrameId .. "Item" .. itemFrameId]
+      local itemLink = C_Container.GetContainerItemLink(bagId, slotId)
+      if IsILvlUpgrade(itemLink) then
+        ShowArrowTexture(itemFrame)
+      else
+        HideArrowTexture(itemFrame)
+      end
+    end
+
   end
 end)
 
 
-
+function IsCombinedBag()
+  return _G["ContainerFrameCombinedBags"]:IsShown()
+end
 
 -- Returns:
--- The bag frame corresponding to the bagId
--- The bag frame id (1 to 6)
+-- true if the item can be equipped and the item ilvl is greater than the currently equipped item ilvl
+-- false otherwise
+function IsILvlUpgrade(itemLink)
+  if not itemLink then return false end
+
+  local invSlotId = GetInvSlotId(itemLink)
+  -- If armor or weapon
+  if invSlotId and invSlotId >= 1 and invSlotId <= 18 then
+    local _, _, _, itemLevel = GetItemInfo(itemLink)
+    local equippedItemLevel = GetEquippedItemLevel(invSlotId)
+    -- If ring, trinket or weapon, check both slots
+    if invSlotId == 11 or invSlotId == 13 or (invSlotId == 16 and not IsTwoHandedWeapon(itemLink)) then
+      local equippedItemLevel2 = GetEquippedItemLevel(invSlotId + 1)
+      if equippedItemLevel2 < equippedItemLevel then
+        equippedItemLevel = equippedItemLevel2
+      end
+    end
+    if itemLevel > equippedItemLevel then
+      return true
+    end
+  end
+  return false
+end
+
+-- Should be used without combined bags
+-- Returns the bag frame id (1 to 6)
 function GetBagFrameId(bagId)
   for bagFrameId = 1, 6 do
     local bagFrame = _G["ContainerFrame" .. bagFrameId]
-
     -- Check if this bag frame id is equal to input param bagId
     -- Because when opening a bag, the same bag can be ContainerFrame1 or 2 or 3 or 4 or 5 or 6
-    -- depending on the order you open the bags (it's not related to the bagId)
+    -- depending on the order you open the bags
     if bagFrame:IsShown() and bagFrame:GetBagID() == bagId then
       return bagFrameId
     end
   end
+  return nil
+end
+
+-- Returns the item level of the equipped item in invSlotId
+function GetEquippedItemLevel(invSlotId)
+  local itemLink = GetInventoryItemLink("player", invSlotId)
+  if itemLink then
+    local _, _, _, itemLevel = GetItemInfo(itemLink)
+    return itemLevel
+  end
+  return 0
 end
 
 local invSlotIds = {
@@ -104,24 +121,37 @@ local invSlotIds = {
   INVTYPE_RELIC = 18,
 }
 
+-- Returns the inventory slot id where the item can be equipped (1 to 18)
+-- Ring, trinket and weapon always returns 11, 13 and 16 (because there are 2 slots)
 function GetInvSlotId(itemLink)
   local _, _, _, _, _, _, _, _, itemEquipLoc = GetItemInfo(itemLink)
   if invSlotIds[itemEquipLoc] then
     return invSlotIds[itemEquipLoc]
   end
+  return nil
 end
 
--- Returns the item level of the equipped item in invSlotId
-function GetEquippedItemLevel(invSlotId)
-  local itemLink = GetInventoryItemLink("player", invSlotId)
-  if itemLink then
-    local _, _, _, itemLevel = GetItemInfo(itemLink)
-    return itemLevel
+function IsTwoHandedWeapon(itemLink)
+  local _, _, _, _, _, _, _, _, itemEquipLoc = GetItemInfo(itemLink)
+  if itemEquipLoc == "INVTYPE_2HWEAPON" then
+    return true
   end
-  return 0
+  return false
 end
 
-function HideArrowFrame(itemFrame)
+-- Show or create the arrow texture
+function ShowArrowTexture(itemFrame)
+  if itemFrame["ILvlUpgradeTex"] then
+    itemFrame["ILvlUpgradeTex"]:Show()
+  else
+    itemFrame["ILvlUpgradeTex"] = itemFrame:CreateTexture(nil, "OVERLAY")
+    itemFrame["ILvlUpgradeTex"]:SetTexture("Interface\\MINIMAP\\MiniMap-QuestArrow")
+    itemFrame["ILvlUpgradeTex"]:SetPoint("TOPRIGHT", itemFrame, "TOPRIGHT", 6, 4)
+    itemFrame["ILvlUpgradeTex"]:SetSize(26, 26)
+  end
+end
+
+function HideArrowTexture(itemFrame)
   if itemFrame["ILvlUpgradeTex"] then
     itemFrame["ILvlUpgradeTex"]:Hide()
   end
